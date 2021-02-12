8d35dfc0e246e2a59408df6ab248ced935549dfc
Leandro Alves <leandrojoapi@gmail.com>
2021-02-12 02:44:27 -0300

    Model - Validação de client e date

--- models/atendimentosModel.js
+++ models/atendimentosModel.js
@@ -9,20 +9,53 @@ class AtendimentosModel {
         const creationDate = moment().format('YYYY-MM-DD HH:MM:SS')
         // Utilizando o moment para formatar a data recebida, indicando o formato que é enviado
         const date = moment(atendimento.date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        // Criando um atendimento atualizando, com tudo que já havia em atendimentos e adicionando a data de
-        // criação e a data recebida
-        const atendimentoDated = {...atendimento, creationDate, date} 
-        // Comando SQL
-        const sql = 'INSERT INTO atendimentos SET ?'
+        
+        // Retorna true se a data recebida é igual o após a data de criação
+        const dateValidator = moment(date).isSameOrAfter(creationDate)
+        // Retorna true se o campo "client" possui mais de 1 caractere
+        const clientValidator = atendimento.client.length > 1 
 
-        // Realizando um query, inserindo todo o conteudo do objeto "atendimentoDated"
-        connection.query(sql, atendimentoDated, (error, results) => {
-            if (error) {
-                res.status(400).json(error)
-            } else {
-                res.status(201).json(results)
+        // Arrya com todas as validações e suas mensagens 
+        const validators = [
+            // Campo date
+            {
+                nome: 'date',
+                valid: dateValidator,
+                message: 'ERROR! Data deve ser maior ou igual a data atual!'
+            },
+            // Campo client
+            {
+                nome: 'client',
+                valid: clientValidator,
+                message: 'ERROR! Nome do cliente deve ser maior que 1 caracterer!'
             }
-        })
+        ]
+
+        // Armazena o objeto em "errors", caso alguma validação possua "false" como "valid"
+        const errors = validators.filter(field => !field.valid)
+        // Verifica quantos erros existem (retorna false caso não exista nenhum)
+        const hasErrors = errors.length
+
+        // Verificando se exite algum erro
+        if (hasErrors) {
+            res.status(400).json(errors)
+        } else {
+            // Criando um atendimento atualizando, com tudo que já havia em atendimentos e adicionando a data de
+            // criação e a data recebida
+            const atendimentoDated = { ...atendimento, creationDate, date }
+
+            // Comando SQL
+            const sql = 'INSERT INTO atendimentos SET ?'
+
+            // Realizando um query, inserindo todo o conteudo do objeto "atendimentoDated"
+            connection.query(sql, atendimentoDated, (error, results) => {
+                if (error) {
+                    res.status(400).json(error)
+                } else {
+                    res.status(201).json(results)
+                }
+            })
+        }
     }
 }
 
