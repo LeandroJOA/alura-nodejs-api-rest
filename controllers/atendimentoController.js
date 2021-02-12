6b695db036ffb8f081d578d07d1d67baa65f5309
Leandro Alves <leandrojoapi@gmail.com>
2021-02-12 01:24:17 -0300

    Controllers - Armazenamento dos dados recebido e chamada do metodo save

--- controllers/atendimentoController.js
+++ controllers/atendimentoController.js
@@ -1,3 +1,5 @@
+const atendimentosModel = require('../models/atendimentosModel')
+
 // Exporta estas rotas, para que sejam visiveis em todo o codigo
 module.exports = app => {
 
@@ -6,7 +8,13 @@ module.exports = app => {
 
     // Rota POST (localhost:3000/atendimentos)
     app.post('/atendimentos', (req, res) => { 
-        console.log(req.body)
+        // Armazenando o body enviado da requisição realizada
+        const atendimento = req.body
+
+        // Chamando o metodo "save" e enviando os dados recebidos
+        atendimentosModel.save(atendimento)
+
+        // Mensagem de resposta
         res.send('Você está na rota de atendimentos e está realizando um POST')
     })
 }
