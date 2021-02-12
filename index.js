d126b035283b89e6860189c1545d5075b6b813af
Leandro Alves <leandrojoapi@gmail.com>
2021-02-12 01:19:52 -0300

    Chamada da classe 'Tables'

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 // Importa o conteudo da config "customExmpress"
 const customExpress = require('./config/customExpress')
 const connection = require('./database/connection')
+const tables = require('./database/tables')
 
 connection.connect(error => {
+    // Caso ocorra algum erro
     if (error) {
         console.log(error)
+    // Caso a query seja bem sucedida    
     } else {
+        // Mensagem de sucesso
         console.log('Conexão com o banco de dados realizada com sucesso!');
+
+        // Chama o metodo de inicialização, enviando a conexão 
+        tables.init(connection)
 
         // Executa a função da customExpress, e armazena o retorno em app
         const app = customExpress()
