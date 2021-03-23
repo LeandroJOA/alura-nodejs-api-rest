cbb52b72fd48b4f9ba3fd7b7ad01cacf0c9db8f8
Leandro Alves <leandrojoapi@gmail.com>
2021-03-23 05:17:36 -0300

    FileUpload - Identificando a extensao do arquivo e definindo tipos validos

--- files/fileUpload.js
+++ files/fileUpload.js
@@ -1,18 +1,34 @@
 // Importanto biblioteca FileSystem
 const fs = require('fs')
+const pathExt = require('path')
 
 module.exports = (path, filename, callbackCreatedImage) => {
     // Metodo assincrono
 
-    // Caminho onde a imagem a irá ser salva
-    const newPath = `./assets/savedImages/${filename}`
+    // Extensões que serão aceitas
+    const validExt = ['jpg', 'png', 'jpeg']
 
-    // Criando uma Stream de leitura da imagem
-    fs.createReadStream(path)
-        // Em seguida, criando uma Stream de escrita deste arquivo
-        .pipe(fs.createWriteStream(newPath))
-        // Ao finalizar, é executado uma função de callback
-        .on('finish', () => callbackCreatedImage(newPath))
+    // Capturando a extensão da imagem a ser armazenada
+    const ext = pathExt.extname(path)
+    
+    // Verificando se a extensão da imagem recebida esta dentro do array de tipos validos (-1 caso não)
+    // substring - Ignora a primeira posição de ext (no caso o ".")
+    const extIsValid = validExt.indexOf(ext.substring(1))
+
+    // Verifica se o tipo é valido
+    if (extIsValid !== -1) {
+        // Caminho onde a imagem a irá ser salva
+        const newPath = `./assets/savedImages/${filename}${ext}`
+
+        // Criando uma Stream de leitura da imagem
+        fs.createReadStream(path)
+            // Em seguida, criando uma Stream de escrita deste arquivo
+            .pipe(fs.createWriteStream(newPath))
+            // Ao finalizar, é executado uma função de callback
+            .on('finish', () => callbackCreatedImage(newPath))
+    } else {
+        console.log("Error! Tipo invalido");
+    }
 }
 
 /*
