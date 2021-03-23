88a7da9fd3feedf5d3f5b3b96acf57193403fdaf
Leandro Alves <leandrojoapi@gmail.com>
2021-03-23 03:58:36 -0300

    Comentarios

--- files/fileUpload.js
+++ files/fileUpload.js
@@ -1,5 +1,9 @@
+// Importanto biblioteca FileSystem
 const fs = require('fs')
 
+// Criando uma Stream de leitura do arquivo "test.jpg"
 fs.createReadStream('./assets/test.jpg')
+    // Em seguida, criando uma Stream de escrita deste arquivo, agora com o nome "testStream.jpg"
     .pipe(fs.createWriteStream('./assets/testStream.jpg'))
+    // Ao finalizar, é executado uma função de callback, com feedback de sucesso
     .on('finish', () => console.log('Imagem foi escrita com sucesso'))
